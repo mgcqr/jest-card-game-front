b34a4b0eff3c270e629cbd254666475a54d5fece
mgcqr <wuyufeii3e@163.com>
2022-01-26 11:03:56 +0800

    Add websocket demo

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <GetJoke></GetJoke>
   <LoginHello></LoginHello>
+  <WebsocketDemo></WebsocketDemo>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 import GetJoke from './components/GetJoke.vue'
 import LoginHello from './components/LoginHello.vue'
+import WebsocketDemo from './components/WebsocketDemo.vue'
 
 export default {
   name: 'App',
   components: {
    // HelloWorld,
     GetJoke,
-    LoginHello
+    LoginHello,
+    WebsocketDemo
   }
 }
 </script>
